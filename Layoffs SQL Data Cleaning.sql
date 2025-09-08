5cdc64e8b8a0205373e74d5ca8af4862400e0a30
Kedonp <126833975+Kedonp@users.noreply.github.com>
2025-09-07 22:26:11 -0700

    Update Layoffs SQL Data Cleaning.sql

--- Layoffs SQL Data Cleaning.sql
+++ Layoffs SQL Data Cleaning.sql
@@ -26,11 +26,13 @@ SELECT *
 FROM world_layoffs.layoffs_v2;
 
 -- 2. Finding duplicate rows
+-- Any row_num with "2" or more is considered a duplicate
 SELECT *,
 ROW_NUMBER() OVER(PARTITION BY company, location, industry, 
 total_laid_off, percentage_laid_off, `date`, stage, country, funds_raised_millions) AS row_num
 FROM world_layoffs.layoffs_v2;
 
+-- Creating a cte to identify duplicate rows
 WITH duplicate_cte AS
 (
 SELECT *,
@@ -47,7 +49,7 @@ SELECT *
 FROM world_layoffs.layoffs_v2
 WHERE company = 'Casper';
 
--- Creating v3 table with new column for coumting duplicates
+-- Creating v3 table with new "row_num" column for counting duplicates
 USE world_layoffs;
 
 CREATE TABLE `layoffs_v3` (
@@ -83,10 +85,11 @@ SELECT *
 FROM world_layoffs.layoffs_v3
 WHERE row_num > 1;
 
--- 3. Checking columns for erros and standardizing data
+-- 3. Checking columns for errors and standardizing data
 SELECT company, TRIM(company)
 FROM world_layoffs.layoffs_v3;
 
+-- Trimming out leading and trailing spaces
 UPDATE world_layoffs.layoffs_v3
 SET company = TRIM(company);
 
@@ -133,7 +136,7 @@ MODIFY COLUMN `date` DATE;
 SELECT `date`
 FROM world_layoffs.layoffs_v3;
 
--- 3. Find null or blank values and filling them appropriately 
+-- 4. Find null or blank values and filling them appropriately 
 SELECT *
 FROM world_layoffs.layoffs_v3
 WHERE total_laid_off IS NULL
@@ -148,6 +151,8 @@ SELECT *
 FROM world_layoffs.layoffs_v3
 WHERE company = 'Airbnb';
 
+-- Find companies with missing industry values by matching them to rows 
+-- of the same company where industry is not null
 SELECT t1.industry, t2.industry
 FROM world_layoffs.layoffs_v3 t1
 JOIN world_layoffs.layoffs_v3 t2
@@ -155,10 +160,13 @@ JOIN world_layoffs.layoffs_v3 t2
 WHERE (t1.industry IS NULL OR t1.industry = '')
   AND t2.industry IS NOT NULL;
 
+-- Replacing blank values with null values so we can fill data
 UPDATE world_layoffs.layoffs_v3
 SET industry = NULL
 WHERE industry = '';
 
+-- Update missing industry values by copying the industry from another row 
+-- of the same company where it is available.
 UPDATE world_layoffs.layoffs_v3 t1
 JOIN world_layoffs.layoffs_v3 t2
 	ON t1.company = t2.company
